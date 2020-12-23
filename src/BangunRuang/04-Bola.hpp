c5f5670b5a50ba3559affb598664dfc968854e83
mahardika denie <dikamahar884@gmail.com>
2020-12-23 20:38:21 +0700

    .

--- src/BangunRuang/04-Bola.hpp
+++ src/BangunRuang/04-Bola.hpp
@@ -12,12 +12,13 @@ class Bola : public BangunRuang{
         void Inputan(){
             cout<<"\n==Bola=="<<endl;
             cout<<"Masukan Nilai Jari - jari : ";cin>>valueA;
+            cout<<"Masukan Nilai Tinggi : ";cin>>valueB ;
         }
         void  Display(){
             Hasil = 1.33 * 3.14 *  valueA * valueA * valueA  ;
             cout<<"\nRumus Volume Lingkaran: "<<endl;
             cout<<"1.33"<<" * "<<"phi"<<" * "<<"Jari - Jari"<<" * "<<"Jari - Jari"<<" * "<<"Tinggi"<<endl;
-            cout<<"1.33"<<"3.14"<<" * "<<valueA<<" * "<<valueA<<" * "<<valueA<<" = "<<Hasil<<endl;
+            cout<<"1.33"<<" * "<<"3.14"<<" * "<<valueA<<" * "<<valueA<<" * "<<valueB<<" = "<<Hasil<<endl;
             cout<<"Volume dari Lingkaran : "<<Hasil<<endl;
         }
 };
